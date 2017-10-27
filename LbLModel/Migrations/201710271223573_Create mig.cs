8c396b14065a1bb49383a12c8156fbbf30320d2e
Your Name <emonsafayet@gmail.com>
2017-10-27 18:32:54 +0600

    added student db package

---
+++ LbLModel/Migrations/201710271223573_Create mig.cs
@@ -0,0 +1,44 @@
+namespace LbLModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Createmig : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Address = c.String(nullable: false),
+                        Phone = c.String(nullable: false, maxLength: 50),
+                        Created = c.DateTime(nullable: false),
+                        CreatedBy = c.String(nullable: false, maxLength: 50),
+                        Modified = c.DateTime(nullable: false),
+                        ModifiedBy = c.String(nullable: false, maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name)
+                .Index(t => t.Phone)
+                .Index(t => t.Created)
+                .Index(t => t.CreatedBy)
+                .Index(t => t.Modified)
+                .Index(t => t.ModifiedBy);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Students", new[] { "ModifiedBy" });
+            DropIndex("dbo.Students", new[] { "Modified" });
+            DropIndex("dbo.Students", new[] { "CreatedBy" });
+            DropIndex("dbo.Students", new[] { "Created" });
+            DropIndex("dbo.Students", new[] { "Phone" });
+            DropIndex("dbo.Students", new[] { "Name" });
+            DropTable("dbo.Students");
+        }
+    }
+}
